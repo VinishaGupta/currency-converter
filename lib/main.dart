2d980c00572b262dff3f8dfe8eec6bec92fef589
Vinisha Gupta <vinishasgupta@gmail.com>
2026-05-03 01:20:47 +0530

    excercise 2:3 boxes in a row

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,23 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
-        body:Column(
+        body: Row(
           children: [
-            Text("hello"),
-            Text("Flutter"),
             Container(
-              height: 100,
               width: 100,
-              color: Colors.red,
-            ),
+              height:100,
+              color:Colors.pink
+            ),Container(
+              width: 100,
+              height:100,
+              color:Colors.green
+            ),Container(
+              width: 100,
+              height:100,
+              color:Colors.blue
+            )
           ],
-        )
+        ),
       ),
     );
   }
