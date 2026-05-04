ca83af80773a8b2df2131998e4f37145737082b5
Vinisha Gupta <vinishasgupta@gmail.com>
2026-05-04 08:58:11 +0530

    exercise5 alignment

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,11 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       home: Scaffold(
         body: Column(
+          
           children: [
             Text("Hello"),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(width: 100,height: 100,color: Colors.pink,),
                 SizedBox(height: 20, width: 20,),
